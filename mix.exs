fd59cabd44d68d374923f195c4f0640ce8c1a52c
Raymond Loranger <Raymond.Loranger@bell.net>
2017-03-29 19:42:38 -0400

    new print_table options: :margins and :header_terms

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IO.ANSI.Table.Mixfile do
   def project do
     [
       app: :io_ansi_table,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -19,7 +19,7 @@ defmodule IO.ANSI.Table.Mixfile do
 
   defp description do
     "Prints data to STDOUT in a table with borders and colors. "
-    <> "Can choose a table style among the 24 predefined ones."
+    <> "Can choose a table style among the 25 predefined ones."
   end
 
   def package do
