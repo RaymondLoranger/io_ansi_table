9876b067ff34782f0ba8abc5679f380673dc12cc
RaymondLoranger <Raymond.Loranger@bell.net>
2025-08-14 17:07:47 -0400

    1.0.39

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IO.ANSI.Table.Mixfile do
   def project do
     [
       app: :io_ansi_table,
-      version: "1.0.38",
+      version: "1.0.39",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "IO ANSI Table",
